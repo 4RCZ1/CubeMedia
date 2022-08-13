65bb2a43b5d2f1cdc3a7b7e348c5b48d8cb2e110
Jager72 <49473040+Jager72@users.noreply.github.com>
2022-08-13 20:15:36 +0200

    Icons v2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,11 +3,11 @@ import FacebookIcon from "../images/contact/FacebookIcon.js";
 import InstagramIcon from "../images/contact/InstagramIcon.js";
 import BehanceIcon from "../images/contact/BehanceIcon.js";
 function Contact() {
-  return <header className={css.header}>
-  <FacebookIcon/>
-  <InstagramIcon/>
-  <BehanceIcon/>
-  </header>;
+  return <div className={css.iconText}>
+  <FacebookIcon/><p>facebook.com/cubesmediainteractive</p>
+  <InstagramIcon/><p>instagram.com/cube_media_interactive</p>
+  <BehanceIcon/><p>behance.net/cube_interactive</p>
+  </div>;
 }
 
 export default Contact;
