8d9a40adc82a92825365f94724587cb1ca105da6
Marcin Alchimowicz <marcinowicz1@gmail.com>
2022-08-14 23:27:34 +0200

    feat: made buttons in slider work, and added switched to mui for handling collapsible

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -7,25 +7,85 @@ import CandC from "../images/slider/CandC.png";
 import Fashnactv from "../images/slider/FASHNACTV.png";
 import Mercedeseqb2022 from "../images/slider/EQB.png";
 import Juwenalia from "../images/slider/JUWENALIA.png";
-import {Fragment, useState} from "react";
+import {Fragment, useEffect, useState} from "react";
 import Collapsible from "react-collapsible";
+import {Accordion, AccordionActions, AccordionDetails, AccordionSummary} from "@mui/material";
 
 const Slider = () => {
+  const components = [FASHNACTV, MERCEDESEQB2022, CARSANDCOFFEE2022, JUWENALIASZCZECIN2022];
   const [project, setProject] = useState(null);
+  const [scrollOffset, setScrollOffset] = useState(0);
   const SetProject = (index) => {
     setProject(project===index ? null : index);
   }
 
-  const components = [FASHNACTV, MERCEDESEQB2022, CARSANDCOFFEE2022, JUWENALIASZCZECIN2022];
   const Component = project===null ? ()=>null : components[project];
   const images = [Fashnactv, Mercedeseqb2022, CandC, Juwenalia];
+
+  useEffect(() => {
+    setScrollOffset(document.getElementsByClassName(css.slider)[0].scrollWidth/components.length);
+    console.log(scrollOffset);
+  }, [components.length, scrollOffset]);
+
+  const goToNext = () => {
+    const slider = document.getElementsByClassName(css.slider)[0];
+    console.log(slider.scrollLeft);
+    console.log(slider.scrollWidth);
+    console.log(scrollOffset);
+    console.log('parent:',slider.parentElement.scrollWidth)
+    if(slider.scrollLeft+slider.parentElement.scrollWidth < slider.scrollWidth) {
+      slider.scrollBy({
+        top: 0,
+        left: scrollOffset,
+        behavior: 'smooth'
+      })
+    }else if(slider.scrollLeft+slider.parentElement.scrollWidth === slider.scrollWidth) {
+      slider.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      })
+    }else{
+      slider.scrollTo({
+        top: 0,
+        left: slider.scrollWidth,
+        behavior: 'smooth'
+      })
+    }
+  }
+
+  const goToPrevious = () => {
+    const slider = document.getElementsByClassName(css.slider)[0];
+    console.log(slider.scrollLeft);
+    if(slider.scrollLeft - scrollOffset > 0) {
+      slider.scrollBy({
+        top: 0,
+        left: -scrollOffset,
+        behavior: 'smooth'
+      })
+    }else if(slider.scrollLeft === 0) {
+      slider.scrollTo({
+        top: 0,
+        left: slider.scrollWidth - scrollOffset,
+        behavior: 'smooth'
+      })
+    }else{
+      slider.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      })
+    }
+  }
+
+
 
 
   return (
     <Fragment>
     <div className={css.sliderContainer}>
-      <div className={css.leftBorder}>
-        <div className="arrow-left icon"></div>
+      <div className={css.leftBorder} onClick={goToPrevious}>
+        <div className={"arrow-left icon "+css.icon}></div>
       </div>
       <div className={css.slider}>
         {images.map((image, index) => {
@@ -35,13 +95,16 @@ const Slider = () => {
           )
         })}
       </div>
-      <div className={css.rightBorder}>
-        <div className="arrow-right icon"></div>
+      <div className={css.rightBorder} onClick={goToNext}>
+        <div className={"arrow-right icon "+css.icon}></div>
       </div>
     </div>
-      <Collapsible trigger={''} open={project!==null} lazyRender={true}>
-        <Component/>
-      </Collapsible>
+      <Accordion expanded={project!==null}>
+        <AccordionSummary></AccordionSummary>
+        <AccordionDetails>
+          <Component/>
+        </AccordionDetails>
+      </Accordion>
     </Fragment>
   )
 }
