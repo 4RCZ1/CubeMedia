9b4b5858da93d531b40b7d9112a69941daa87341
Jager72 <49473040+Jager72@users.noreply.github.com>
2022-08-17 14:36:12 +0200

    Feat: Added template for video section

--- src/components/OfferItems/FOTO.js
+++ src/components/OfferItems/FOTO.js
@@ -1,8 +1,19 @@
+import css from "../../styles/foto.module.scss"
+import foto from "../../images/offer/FotoContent1.jpg";
 const FOTO = () => {
     return (
       <div>
-
+      <div className={css.title}>
+        <p>
+          <b>CUBE MEDIA</b> OFERTA FOTO
+        </p>
       </div>
+      <div className={css.content}>
+          <div className={css.fotoWrapper}><img src={foto} className={css.foto}></img></div>
+          <div className={css.fotoWrapper}><img src={foto} className={css.foto}></img></div>
+          <div className={css.fotoWrapper}><img src={foto} className={css.foto}></img></div>
+      </div>
+  </div>
     )
   }
   export default FOTO;
