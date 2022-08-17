207f829f4eebfea8111a6161fd3b1e2957603dd0
Jager72 <49473040+Jager72@users.noreply.github.com>
2022-08-17 13:54:46 +0200

    Fix: Video section now is set up according to project design

--- src/components/OfferItems/VIDEO.js
+++ src/components/OfferItems/VIDEO.js
@@ -9,13 +9,15 @@ const VIDEO = () => {
             
                 <p> Cube media oferuje szeroki zakres usług z zakresu realizacji oraz 
                     montażu filmów na wszelkie social media. </p>
-                    
+                <span className={css.pricesText}>
                 <p>  Film z eventu muzycznego do 10 min 1000zł </p>
                 <p>  Nagranie koncertu do 60 min 1500zł </p>
                 <p>  Spot reklamowy dla firmy do 7 min 2500zł </p>
                 <p>  Teledysk muzyczny od 1500zł </p>
                 <p>  Spot z wydarzenia od 1400zł za 15 min film </p>
                 <p>  Wywiad na social media 750zł za 5min </p>
+                </span>    
+                
                     
                 <p className={css.smallText}> *Uwaga podane ceny nie są cenami ostateczymi. Aby uzyskać konkretną
                     wycenę prosimy o kontak mailowy lub telefoniczny wszelkie niezbędne
